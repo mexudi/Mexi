9796901150a8c1bd9b142855975ab5259ff2e25d
mexudi <soufianmexudi20@gmail.com>
2023-02-20 18:05:01 +0100

    add record audion function

---
+++ audio_record.py
@@ -0,0 +1,22 @@
+def recordAudio():
+    # record the audio
+    r = sr.Recongnizer() #creating a recongnizer object
+
+    # Open the microphone and start recording
+    with sr.Microphone() as source:
+        print('Say something!')
+        audio = r.listen(source)
+
+    # Use Googles speecg recognition
+
+    data = ''
+    try:
+        data = r.recognize_google(audio)
+        print('You said: '+data)
+    except sr.UnknownValueError:
+        print('Google Speech Recognition could not understand the audio, unkown error')
+    except sr.RequestError as e:
+        print('request results from Google Speech recognition service error '+e)
+
+
+    return data
