81b00ccbc153a8bd5f022d86f1d72c041fd6f53f
mexudi <soufianmexudi20@gmail.com>
2023-02-20 18:14:22 +0100

    test recortd audio function

---
+++ main.py
@@ -0,0 +1,15 @@
+import speech_recongnition as sr
+import sr
+from gtts import gTTS
+import datetime
+import warnings
+import calendar
+import random
+import wikipedia
+import recordAudio from audio_record
+
+# Ignore any warning messages
+warning.filterwarnings('ignore')
+
+# Test record audio
+recordAudio()
