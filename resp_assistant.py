4e03a01af6cfe2d61486efc3dbaca37cd603e821
mexudi <soufianmexudi20@gmail.com>
2023-02-21 15:16:08 +0100

    add response assistant function

---
+++ resp_assistant.py
@@ -0,0 +1,11 @@
+def assistaneResponse(text):
+    print(text)
+
+    # COnvert the text to speech
+    myobj = gTTS(text=text, lang='en', slow=False)
+
+    # Save the converted audio to a file
+    myobj.save('assistant_response.mp3')
+
+    # Play the converted file
+    os.system('start assistant_response.mp3')
