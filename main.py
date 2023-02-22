e376fdac7523c2809fc0b8733cf68c18f3d36f73
mexudi <soufianmexudi20@gmail.com>
2023-02-22 21:27:34 +0100

    import wake words function in main

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import calendar
 import random
 import wikipedia
 from audio_record import recordAudio
-from resp_assistant import assistaneResponse 
+from resp_assistant import assistaneResponse
+from wake_words.py import  wakeWord 
 
 # Ignore any warning messages
 warnings.filterwarnings('ignore')
